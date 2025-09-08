bdca5ff053bc09f5b8a4f1e2139dd588e7c42aa4
rafaeldossantos <rafael.santoss2802@gmail.com>
2025-09-08 11:25:31 -0300

    arrumei os getter and setter

--- src/main/java/com/example/projetoPresencial/model/ClienteModel.java
+++ src/main/java/com/example/projetoPresencial/model/ClienteModel.java
@@ -9,7 +9,8 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.util.List;
 
-  @AllArgsConstructor
+@Getter @Setter
+@AllArgsConstructor
 public class ClienteModel {
     List<ContaModel> contas;
 
@@ -17,44 +18,4 @@ public class ClienteModel {
     private Long Id;
     private String nome, cpf, email;
     private LocalDate nadaNascimento;
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getNadaNascimento() {
-        return nadaNascimento;
-    }
-
-    public void setNadaNascimento(LocalDate nadaNascimento) {
-        this.nadaNascimento = nadaNascimento;
-    }
 }
