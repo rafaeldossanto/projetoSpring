11bbbb97c58ed2367e96ac2e5cbc08b57edc0d12
rafaeldossantos <rafael.santoss2802@gmail.com>
2025-09-08 10:32:17 -0300

    Spring Base

---
+++ src/main/java/com/example/projetoPresencial/ProjetoPresencialApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoPresencial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPresencialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPresencialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetoPresencial/ProjetoPresencialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoPresencial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPresencialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
